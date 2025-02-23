35f2d451abe734de89da365124074dc5a7bb0cd8
AhmedMohamed878 <am3753828@gmail.com>
2025-02-23 17:25:50 +0200

    Add files via upload

---
+++ Weather Advisor.py
@@ -0,0 +1,13 @@
+t=int(input("Enter the Temperature in Celsius: "))
+
+if t>=30:
+    print("It's a hot day. Stay hydrated!")
+    
+elif t>=20:
+    print("It's a warm day. Enjoy the weather!")
+    
+elif t>=10:
+    print("It's a cool day. Wear a jacket!")
+    
+else:
+    print("It's a cold day. Stay warm!")
